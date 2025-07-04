9129c75559450cdd23f40cd89188d92facc98fd4
Cardyhue <lucoulucas@gmail.com>
2025-07-04 11:50:31 -0300

    Adicionando estilização para o rodapé de todas as paginas

---
+++ style.css
@@ -0,0 +1,16 @@
+.footer {
+  background-color: #141414;
+  color: white;
+  text-align: center;
+  padding: 1.5rem;
+  margin-top: 2rem;
+}
+
+/* Media Queries para Responsividade */
+
+@media (max-width: 480px) {
+  .footer {
+    font-size: 0.9rem;
+    padding: 1rem;
+  }
+}
